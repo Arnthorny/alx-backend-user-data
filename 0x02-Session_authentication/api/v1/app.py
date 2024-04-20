b7bb16ec17031b2741bdfe58b7712ebb99d63c69
Arnthorny <chidubem.anthony26@gmail.com>
2024-04-20 08:33:55 -0700

    Missing backslash before multiline

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,8 +39,8 @@ def auth_before_fn() -> None:
                  '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excl_path):
         return
-    elif auth.authorization_header(request) is None and
-    auth.session_cookie(request) is None:
+    elif auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
         abort(401)
     elif auth.current_user(request) is None:
         abort(403)
